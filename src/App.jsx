f91dae03dcc465e0eb380d41cf5093139fb5759b
Dylon <dmychel@tutanota.com>
2024-11-29 22:09:04 -0500

    drag div logic writtten with use-gesture and react spring

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useDrag } from "@use-gesture/react";
+import { useSpring, animated } from "@react-spring/web";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const divPos = useSpring({ x: 0, y: 0 });
+  const bindDivPos = useDrag((params) => {
+    divPos.x.set(params.offset[0]);
+    divPos.y.set(params.offset[1]);
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <animated.div
+        {...bindDivPos()}
+        style={{
+          touchAction: "none",
+          y: divPos.y,
+          x: divPos.x,
+        }}
+      >
+        <h1>hello world</h1>
+      </animated.div>
+    </div>
+  );
 }
 
-export default App
+export default App;
