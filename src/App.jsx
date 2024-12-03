7d39bc759da8933d1741dd5c40685cfb724db185
dmychel <dmychel@tutanota.com>
2024-12-03 13:49:28 -0500

    Removed jsx of main tab and turned it into its own component
    updated imports for other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,27 @@
-import { useDrag } from "@use-gesture/react";
-import { useSpring, animated } from "@react-spring/web";
+import { useState } from "react";
+
+// components
+import AboutTab from "./tabs/AboutTab";
+import ContactTab from "./tabs/ContactTab";
+import MainTab from "./tabs/MainTab";
+import WebDevTab from "./tabs/WebDevTab";
+
+// styling
 import "./App.css";
 
 function App() {
-  const divPos = useSpring({ x: 0, y: 0 });
-  const bindDivPos = useDrag((params) => {
-    divPos.x.set(params.offset[0]);
-    divPos.y.set(params.offset[1]);
-  });
+  const [tabArr, setTabArr] = useState([MainTab]);
+
+  function openTab(type) {
+    setTabArr([...tabArr, type]);
+  }
 
   return (
     <div className="app">
-      <animated.div
-        {...bindDivPos()}
-        style={{
-          touchAction: "none",
-          y: divPos.y,
-          x: divPos.x,
-        }}
-        className="main_tab"
-      >
-        <div className="title">
-          <h1>hello world</h1>
-          <div className="sub_title">
-            <span>Artist, Web Developer, and Dabbler</span>
-          </div>
-        </div>
-        <nav>
-          <ul>
-            <li>about</li>
-            <li>web dev</li>
-            <li>photography</li>
-            <li>links</li>
-            <li>contact</li>
-          </ul>
-        </nav>
-      </animated.div>
+      {tabArr.map((tab, index) => {
+        const TabName = tab;
+        return <TabName key={index} openTab={openTab} AboutTab={AboutTab} />;
+      })}
     </div>
   );
 }
