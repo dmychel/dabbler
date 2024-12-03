0c940eebe8f92aac4ed5c1b56e7e5b616a2915f3
dmychel <dmychel@tutanota.com>
2024-12-03 13:59:23 -0500

    jsx update

--- src/tabs/ContactTab.jsx
+++ src/tabs/ContactTab.jsx
@@ -1,3 +1,24 @@
+// npm
+import { useDrag } from "@use-gesture/react";
+import { useSpring, animated } from "@react-spring/web";
+
 export default function Contact() {
-  return;
+  const divPos = useSpring({ x: 0, y: 0 });
+  const bindDivPos = useDrag((params) => {
+    divPos.x.set(params.offset[0]);
+    divPos.y.set(params.offset[1]);
+  });
+  return (
+    <animated.div
+      {...bindDivPos()}
+      style={{
+        touchAction: "none",
+        y: divPos.y,
+        x: divPos.x,
+      }}
+      className="about_tab"
+    >
+      Contact
+    </animated.div>
+  );
 }
--- src/tabs/WebDevTab.jsx
+++ src/tabs/WebDevTab.jsx
@@ -1,3 +1,25 @@
+// npm
+import { useDrag } from "@use-gesture/react";
+import { useSpring, animated } from "@react-spring/web";
+
 export default function WebDevTab() {
-  return;
+  const divPos = useSpring({ x: 0, y: 0 });
+  const bindDivPos = useDrag((params) => {
+    divPos.x.set(params.offset[0]);
+    divPos.y.set(params.offset[1]);
+  });
+
+  return (
+    <animated.div
+      {...bindDivPos()}
+      style={{
+        touchAction: "none",
+        y: divPos.y,
+        x: divPos.x,
+      }}
+      className="about_tab"
+    >
+      Web Dev
+    </animated.div>
+  );
 }
